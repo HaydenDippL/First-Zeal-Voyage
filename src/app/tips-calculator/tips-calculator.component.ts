26bf8118e6a0a90be701427a72d225662e91a2eb
Hayden Dippel <hdippel@wisc.edu>
2024-06-03 13:33:04 -0500

    generated base components

---
+++ src/app/tips-calculator/tips-calculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tips-calculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './tips-calculator.component.html',
+  styleUrl: './tips-calculator.component.css'
+})
+export class TipsCalculatorComponent {
+
+}
