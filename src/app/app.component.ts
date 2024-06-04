f5b8c1163441d7d42a25a69537b4555bcefec46e
Hayden Dippel <hdippel@wisc.edu>
2024-06-04 12:57:07 -0500

    created tips calculator skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TipsCalculatorComponent } from './tips-calculator/tips-calculator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TipsCalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/tips-calculator/tips-calculator.component.ts
+++ src/app/tips-calculator/tips-calculator.component.ts
@@ -1,12 +1,65 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
+import { FormsModule } from '@angular/forms';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DropdownModule } from 'primeng/dropdown';
+import { CheckboxModule } from 'primeng/checkbox';
+import { SliderModule } from 'primeng/slider';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { state_sales_taxes, StateSalesTax } from './state-sales-tax';
 
 @Component({
   selector: 'app-tips-calculator',
-  standalone: true,
-  imports: [],
+  standalone: true, 
+  imports: [
+    CardModule,
+    CommonModule,
+    FormsModule,
+    DropdownModule,
+    InputNumberModule,
+    CheckboxModule,
+    SliderModule,
+    InputTextModule
+  ],
   templateUrl: './tips-calculator.component.html',
   styleUrl: './tips-calculator.component.css'
 })
 export class TipsCalculatorComponent {
+  state_sales_taxes: StateSalesTax[] = state_sales_taxes;
+  selected_state: StateSalesTax | undefined;
+  price: number | undefined;
+  tax_rate: number | undefined;
+  include_tax_in_tip: boolean = false;
+  tip_rate: number = 20;
 
+  calculate_tax(): number {
+    return (this.price ?? 0) * (this.tax_rate ?? 0) / 100;
+  }
+  calculate_tip(): number {
+    return (this.price ?? 0) * (this.tip_rate / 100);
+  }
+  calculate_total(): number {
+    let tax: number = this.calculate_tax();
+    let tip: number = this.calculate_tip();
+    return tax + tip;
+  }
+
+  nullify_tax_rate(): void {
+    this.tax_rate = undefined;
+  }
+
+  nullify_state(): void {
+    this.selected_state = undefined;
+  }
+
+  nullify_tax_rate_and_state(): void {
+    this.nullify_tax_rate();
+    this.nullify_state();
+  }
+
+  set_tax_rate(): void {
+    this.tax_rate = this.selected_state!.tax;
+  }
 }
