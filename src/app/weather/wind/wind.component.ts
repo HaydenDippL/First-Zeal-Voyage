9ea6c948b252b930b5eb328e54dfa6f154e6ac9f
Hayden Dippel <haydendippel@Zeals-MacBook-Pro-2.local>
2024-06-17 15:00:36 -0500

    initialized wind component

---
+++ src/app/weather/wind/wind.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wind',
+  standalone: true,
+  imports: [],
+  templateUrl: './wind.component.html',
+  styleUrl: './wind.component.css'
+})
+export class WindComponent {
+
+}
