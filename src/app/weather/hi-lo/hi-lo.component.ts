46cfb8b030a83f683aeea9c3a6d09f19e693ec29
ianlee2024 <ian.lee@zealitconsultants.com>
2024-06-18 15:28:37 -0500

    hi lo and humidity

---
+++ src/app/weather/hi-lo/hi-lo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hi-lo',
+  standalone: true,
+  imports: [],
+  templateUrl: './hi-lo.component.html',
+  styleUrl: './hi-lo.component.css'
+})
+export class HiLoComponent {
+
+}
