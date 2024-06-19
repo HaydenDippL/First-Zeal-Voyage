ca7887f7c288d061317a800275cd434041ee4af4
Hayden Dippel <haydendippel@Zeals-MacBook-Pro-2.local>
2024-06-19 09:20:30 -0500

    began integration of components

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { CityHeaderComponent } from './city-header/city-header.component';
+import { HumidityComponent } from './humidity/humidity.component';
+import { WindComponent } from './wind/wind.component';
+import { HiLoComponent } from './hi-lo/hi-lo.component';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [],
+  imports: [
+    CityHeaderComponent,
+    HumidityComponent,
+    WindComponent,
+    HiLoComponent
+  ],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css'
 })
