189c1be7b5d8647621d1f95494e3467b6cc2d725
Hayden Dippel <haydendippel@Zeals-MacBook-Pro-2.local>
2024-06-19 14:21:28 -0500

    styled weather temperature chart

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -3,7 +3,7 @@ import { CityHeaderComponent } from './city-header/city-header.component';
 import { HumidityComponent } from './humidity/humidity.component';
 import { WindComponent } from './wind/wind.component';
 import { HiLoComponent } from './hi-lo/hi-lo.component';
-
+import { TemperatureChartComponent } from './temperature-chart/temperature-chart.component';
 @Component({
   selector: 'app-weather',
   standalone: true,
@@ -11,7 +11,8 @@ import { HiLoComponent } from './hi-lo/hi-lo.component';
     CityHeaderComponent,
     HumidityComponent,
     WindComponent,
-    HiLoComponent
+    HiLoComponent,
+    TemperatureChartComponent
   ],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css'
